a3ea2c409c56dab9fdf1f6bc4aecb6ca9e7636f3
Alex Cabo <alex.alfara95@gmail.com>
2024-03-08 15:12:37 +0100

    feature: extending typescript interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,20 +8,20 @@ export interface apiMovieType {
 }
 
 export interface Movie { 
-    adult: boolean 
-    backdrop_path: string 
+    adult: boolean
+    backdrop_path: string
     genre_ids: [number] 
     id: number
     original_language: string
     original_title: string
     overview: string 
-    popularity: number 
-    poster_path: string 
-    release_date: string 
-    title: string 
-    video: boolean 
-    vote_average: number 
-    vote_count: number 
+    popularity: number
+    poster_path: string
+    release_date: string
+    title: string
+    video: boolean
+    vote_average: number
+    vote_count: number
 }
 
 export interface apiGenreType {
@@ -31,12 +31,9 @@ export interface apiGenreType {
 export interface Genre {
     id: number
     name: string
-    isActive: boolean
 }
 
-export interface SelectItem {
-    id: number
-    name: string
+export interface SelectItem extends Genre {
     isActive: boolean
 }
 
