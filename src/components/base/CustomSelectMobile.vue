97922c758da453b3d718e6009611b2b3c2915912
Alex Cabo <alex.alfara95@gmail.com>
2024-04-04 17:46:55 +0200

    feature: create new component for mobile to change genre filter for movies

---
+++ src/components/base/CustomSelectMobile.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { SelectItem } from '@/types/globals'
+
+// const emit = defineEmits(['onToggleOption'])
+
+defineProps<{
+  options: Array<SelectItem>
+}>()
+
+
+</script>
+<template>
+  <ul class="flex justify-center flex-wrap">
+    <li
+      v-for="option in options"
+      :key="option.id"
+    >
+      {{ option.name }}
+    </li>
+  </ul>
+</template>
