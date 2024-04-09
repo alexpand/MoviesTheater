24e953211e4833af7bafccce6fb343e50a2910e1
Alex Cabo <alex.alfara95@gmail.com>
2024-04-09 17:44:12 +0200

    feature: fixed mobile responsive in movie details

--- src/views/MovieDetailsView.vue
+++ src/views/MovieDetailsView.vue
@@ -26,8 +26,8 @@ onMounted(async () => {
 
 <template>
   <section>
-    <article class="flex gap-4 p-4">
-      <div class="w-96 flex justify-center">
+    <article class="block sm:flex sm:gap-4 sm:p-4 mb-4">
+      <div class="sm:w-96 flex justify-center mb-4 sm:mb-0">
         <img
           :src="getPosterUrl(`${movie_details?.poster_path}`)"
           :alt="`Movie: ${movie_details?.title}`"
@@ -35,10 +35,10 @@ onMounted(async () => {
           onerror="this.src='/broken_img.svg'"
         />
       </div>
-      <div>
-        <div class="flex">
-          <h1>{{ movie_details?.title }}</h1>
-          <h2>{{ movie_details?.release_date }}</h2>
+      <div class="flex flex-col gap-4">
+        <div class="flex sm:items-center sm:flex-row flex-col gap-4">
+          <h1 class="font-bold text-3xl">{{ movie_details?.title }}</h1>
+          <h2 class="text-2xl">{{ movie_details?.release_date.substring(0,4) }}</h2>
         </div>
         <p>{{ movie_details?.overview }}</p>
         <div v-if="movie_details">
@@ -55,6 +55,7 @@ onMounted(async () => {
         </div>
       </div>
     </article>
+
     <CastsScroller :casts="movie_details?.casts?.cast">
       <router-link :to="`/movie/cast/${id}`" class="block w-full h-full">
         <button @click="setCasts(movie_details?.casts)" class="block w-full h-full">
