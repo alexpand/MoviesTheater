f9c8a1100149acb15bf8653b544a807b3fcb718e
Alex Cabo <alex.alfara95@gmail.com>
2024-04-09 17:30:57 +0200

    feature: modified style and z-index of nav and navlinks

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -11,27 +11,25 @@ function toggle() {
 </script>
 
 <template>
-  <header class="container mx-auto sticky top-0 sm:static shadow-md sm:shadow-none">
+  <header class="container mx-auto sticky top-0 sm:static shadow-md sm:shadow-none z-50">
     <nav class="w-full py-4 sm:bg-transparent bg-red-800">
       <div class="hidden justify-between w-full sm:flex px-4">
         <RouterLink to="/" class="text-red-800 hover:text-white font-bold text-lg">Home</RouterLink>
         <RouterLink to="/login" class="text-red-800 hover:text-white font-bold text-lg">Login</RouterLink>
       </div>
-      <div>
-        <img 
-          src="/menu.svg" 
-          class="h-8 w-8 block sm:hidden mx-4"
-          @click="toggle"
-        >
-        <ul v-if="showMenu" class="fixed top-16 w-full shadow-xl">
-          <li class="py-2 pl-4 bg-red-800/40">
-            <RouterLink to="/" class="font-bold">Home</RouterLink>
-          </li>
-          <li class="py-2 pl-4 bg-red-800/40">
-            <RouterLink to="/login" class="font-bold">Login</RouterLink>
-          </li>
-        </ul>
-      </div>
+      <img 
+        src="/menu.svg" 
+        class="h-8 w-8 block sm:hidden mx-4"
+        @click="toggle"
+      >
+      <ul v-if="showMenu" class="fixed top-16 w-full shadow-xl bg-red-800/80">
+        <li class="py-2 pl-4">
+          <RouterLink to="/" class="font-bold">Home</RouterLink>
+        </li>
+        <li class="py-2 pl-4">
+          <RouterLink to="/login" class="font-bold">Login</RouterLink>
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
