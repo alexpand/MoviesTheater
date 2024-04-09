ca9a69585e0383f388bac6af1a7f885df636a386
Alex Cabo <alex.alfara95@gmail.com>
2024-04-09 17:30:20 +0200

    feature: fixed scroll up button to bottom right always visible for movies view

--- src/components/base/ScrollUpButton.vue
+++ src/components/base/ScrollUpButton.vue
@@ -7,7 +7,7 @@ const { scrollUp } = useGlobalHelper()
 </script>
 
 <template>
-  <button @click="scrollUp" class="h-12 w-12 p-3 rounded-full shadow-lg bg-gray-800 mt-4 sm:hidden block ml-auto">
+  <button @click="scrollUp" class="h-12 w-12 p-3 rounded-full shadow-lg bg-gray-800 mt-4 sm:hidden fixed bottom-2 right-2">
     <img src="/chevron.svg">
   </button>
 </template>
